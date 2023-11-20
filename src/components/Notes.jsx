fec92bc0f8bb974b080b9d1a1e171bf81e7be07f
Zrazhevskii <vbn-134@yandex.ru>
2023-11-20 20:34:58 +0300

    добавление логики при отсутствии заметок

--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -5,25 +5,41 @@ import PropTypes from 'prop-types';
 export default function Notes(props) {
     const { data, onUpdate, onDelet } = props;
 
-    return (
-        <div className='notes-wrapper'>
-            <div className='title-notes'>
-                <h2 className='title-box'>Notes</h2>
-                <button className='notes-btn' onClick={onUpdate}>
-                    🔄
-                </button>
-            </div>
-            <div className='notes'>
-                {data.map((item) => {
-                    return <Note item={item} key={item.id} onDelet={onDelet} />;
-                })}
-            </div>
-        </div>
-    );
+    return (() => {
+        if ( data.length === 0) {
+            return (
+                <div className='nullNotes-wrappes'>
+                    <h2 className='nullNotes-title'>Заметок нет, что-нибудь напишите?</h2>
+                </div>
+            );
+        } else {
+            return (
+                <div className='notes-wrapper'>
+                    <div className='title-notes'>
+                        <h2 className='title-box'>Notes</h2>
+                        <button className='notes-btn' onClick={onUpdate}>
+                            🔄
+                        </button>
+                    </div>
+                    <div className='notes'>
+                        {data.map((item) => {
+                            return (
+                                <Note
+                                    item={item}
+                                    key={item.id}
+                                    onDelet={onDelet}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+            );
+        }
+    })();
 }
 
 Notes.propTypes = {
-  data: PropTypes.array.isRequired,
-  onUpdate: PropTypes.func.isRequired,
-  onDelet: PropTypes.func.isRequired,
-}
+    data: PropTypes.array,
+    onUpdate: PropTypes.func.isRequired,
+    onDelet: PropTypes.func.isRequired,
+};
